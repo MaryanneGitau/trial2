8183a2829fb589e209d44d3b5ea44b2d9b7138bd
MaryanneGitau <maryannembeere@gmail.com>
2024-02-23 18:24:02 +0300

    first commit

---
+++ user_input.py
@@ -0,0 +1,13 @@
+# user_input.py
+
+# Ask the user for their name
+name = input("Maryanne ")
+
+# Ask the user for their age
+age = input("25 ")
+
+# Ask the user for their location
+location = input("Kiambu ")
+
+# Print the collected information
+print(f"Hello, {name}! You are {age} years old and located in {location}.")
